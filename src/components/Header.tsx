7e924afbd430a9ca33a68cb85d4162c073f8b7f3
Franco Armando <francoarmando1911@gmail.com>
2024-12-17 16:42:26 -0300

    rerfactorizando el header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,24 +2,25 @@ export default function Header() {
     return (
         <>
             <header className="relative overflow-hidden">
-                <div className="bg-[#000B54] h-[700px] sm:h-[700px] md:h-[800px] lg:h-[900px] bg-cover bg-center">
-                    <div className="container mx-auto px-6 py-20">
+                <div className="bg-gradient-to-r from-blue-900 to-purple-900 h-[500px]">
+                    <div className="container mx-auto px-6 py-16">
                         <img
-                            src="./icono.png"
+                            src="./logo2.png"
                             alt="INTRA SOFTWARE"
-                            className="w-54 sm:w-32 md:w-48 lg:w-64 xl:w-72 mb-8 mx-auto"
+                            className="w-20 sm:w-24 md:w-28 lg:w-32 xl:w-36 mb-6 mx-auto"
                         />
-                        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 leading-tight text-white text-center">
+                        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 leading-tight text-white text-center">
                             Transformando tus ideas
                             <br />
                             en realidad digital
                         </h1>
-                        <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-gray-300 max-w-2xl mx-auto text-center">
+                        <p className="text-sm sm:text-base md:text-lg lg:text-xl text-gray-300 max-w-2xl mx-auto text-center">
                             Creamos soluciones innovadoras para que tu negocio crezca y se destaque
                         </p>
                     </div>
                 </div>
             </header>
         </>
+
     );
 }
